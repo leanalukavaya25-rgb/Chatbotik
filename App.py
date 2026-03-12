506c41a509b91325fc75866035cf9b0170bf728a
leanalukavaya25-rgb <leana.lukavaya25@lumio.school>
2026-03-12 16:07:09 +0200

    Update App.py

--- App.py
+++ App.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 
 # Page settings
-st.set_page_config(page_title="Find Your Hobby", page_icon="🎨", layout="centered")
+st.set_page_config(page_title="Find Your Hobby", page_icon="༺♰༻", layout="centered")
 
 # Load Logo
 logo = Image.open("Logo.png")
